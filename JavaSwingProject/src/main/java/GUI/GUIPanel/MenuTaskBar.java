1c5d9dc020eebf99523a8d39e591995f0caa6965
MinhVi2004 <141652938+MinhVi2004@users.noreply.github.com>
2024-04-09 23:23:29 +0700

    Add files via upload

---
+++ JavaSwingProject/src/main/java/GUI/GUIPanel/MenuTaskBar.java
@@ -0,0 +1,69 @@
+package GUI.GUIPanel;
+import GUI.GUIThanhPhan.ButtonCustom;
+import javax.swing.*;
+
+
+import java.awt.*;
+import lombok.*;
+@Data
+
+public class MenuTaskBar extends JPanel{
+    private String linkToIMG = "C:\\Users\\dvmv2\\OneDrive\\Documents\\Nam_2\\Fixx\\Java-Swing-main-9-4\\Java-Swing-main\\JavaSwingProject\\src\\main\\java\\Resources";
+    private ButtonCustom btn_TrangChu, btn_sanPham, btn_loaiSP;
+    private ButtonCustom btn_NhapHang, btn_PhieuNhap;
+    private ButtonCustom btn_XuatHang, btn_PhieuXuat;
+    private ButtonCustom btn_TaiKhoan,btn_ThongKe, btn_DoiThongTin, btn_DangXuat;
+
+    public MenuTaskBar() {
+        this.setPreferredSize(new Dimension(288,800));
+        this.setBackground(Color.WHITE);
+        this.setLayout(new BorderLayout(0,0));
+        
+        JPanel thongTinUser= new JPanel(new BorderLayout(0,0));
+        thongTinUser.setBackground(Color.WHITE);
+        thongTinUser.setPreferredSize(new Dimension(288,120));
+        JLabel hoTen= new JLabel("Tên Đăng Nhập");
+        hoTen.setVerticalAlignment(JLabel.CENTER);
+        hoTen.setHorizontalAlignment(JLabel.CENTER);
+        hoTen.setIcon(new ImageIcon("C:\\Users\\Admin\\OneDrive\\Documents\\NetBeansProjects\\JavaHung\\JavaHung\\src\\main\\java\\Resources\\iconUser64.png"));
+        JPanel bar1= new JPanel();
+        bar1.setBackground(Color.BLACK);//new Color(204,214,219));
+        bar1.setPreferredSize(new Dimension(1,0));
+        JPanel bar2= new JPanel();
+        bar2.setBackground(Color.BLACK);//new Color(204,214,219));
+        bar2.setPreferredSize(new Dimension(0,1));
+        JPanel bar3= new JPanel();
+        bar3.setBackground(Color.BLACK); //Color(204,214,219));
+        bar3.setPreferredSize(new Dimension(1,0));
+        
+        
+        thongTinUser.add(hoTen,BorderLayout.CENTER);
+        thongTinUser.add(bar1,BorderLayout.EAST);
+        thongTinUser.add(bar2,BorderLayout.SOUTH);
+        this.add(bar3,BorderLayout.EAST);
+        this.add(thongTinUser,BorderLayout.NORTH);
+        this.setPreferredSize(new Dimension(200,900));
+        // Chuyển thành giao diện sản phẩm
+        JPanel dieuHuong= new JPanel(new GridLayout(11,1,0,0));// giữa, khoảng cách chiều ngang là 0, khoảng cách dọc là 5
+        dieuHuong.setBackground(Color.WHITE);
+        btn_TrangChu= new ButtonCustom("Trang chủ",linkToIMG + "\\trangchu.png");
+        btn_sanPham= new ButtonCustom("Sản phẩm",linkToIMG + "\\sanpham.png");
+        btn_loaiSP= new ButtonCustom("Loại Sản phẩm",linkToIMG + "\\sanpham.png");
+        btn_NhapHang = new ButtonCustom("Nhập hàng",linkToIMG + "\\nhaphang.png");
+        btn_PhieuNhap= new ButtonCustom("Phiếu nhập",linkToIMG + "\\phieunhap.png");
+        btn_XuatHang = new ButtonCustom("Xuất hàng", linkToIMG + "\\xuathang.png");
+        btn_PhieuXuat= new ButtonCustom("Phiếu xuất",linkToIMG + "\\phieuxuat.png");
+        btn_TaiKhoan= new ButtonCustom("Tài khoản",linkToIMG + "\\taikhoan.png");
+        btn_ThongKe= new ButtonCustom("Thống kê",linkToIMG + "\\thongke.png");
+        btn_DoiThongTin= new ButtonCustom("Đổi thông tin",linkToIMG + "\\thongtin.png");
+        btn_DangXuat= new ButtonCustom("Đăng xuất",linkToIMG + "\\dangxuat.png");
+        dieuHuong.add(btn_TrangChu); dieuHuong.add(btn_sanPham); dieuHuong.add(btn_loaiSP);
+        dieuHuong.add(btn_NhapHang); dieuHuong.add(btn_PhieuNhap);
+        dieuHuong.add(btn_XuatHang); dieuHuong.add(btn_PhieuXuat); 
+        dieuHuong.add(btn_TaiKhoan); dieuHuong.add(btn_ThongKe);
+        dieuHuong.add(btn_DoiThongTin); dieuHuong.add(btn_DangXuat);
+        this.add(dieuHuong,BorderLayout.CENTER);
+    }
+    
+    
+}
