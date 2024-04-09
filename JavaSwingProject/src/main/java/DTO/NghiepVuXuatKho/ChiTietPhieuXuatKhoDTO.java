18c045896d9a9e44c0d76043827322cad80d90f0
MinhVi2004 <141652938+MinhVi2004@users.noreply.github.com>
2024-04-09 15:00:49 +0700

    Add files via upload

---
+++ JavaSwingProject/src/main/java/DTO/NghiepVuXuatKho/ChiTietPhieuXuatKhoDTO.java
@@ -0,0 +1,28 @@
+package DTO.NghiepVuXuatKho;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+/************************************************************
+ * @Data = @Setter + @Getter + @RequiredArgsConstructor + .....
+ *          @Setter: Lombok tự động viết toàn bộ Setter cho class
+ *          @Getter: Lombok tự động viết toàn bộ Getter cho class
+ *          @RequiredArgsConstructor: Dùng để tạo các constructor có các tham số chỉ định
+ *              Sử dụng @NonNull để chỉ định trường đặc biệt
+ *******************************************/
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChiTietPhieuXuatKhoDTO {
+      
+    private Integer maSanPham;
+
+    private Integer donGia;
+
+    private Integer maPhieu;
+
+    private Integer soLuong;
+
+    private long thanhTien;
+
+}
