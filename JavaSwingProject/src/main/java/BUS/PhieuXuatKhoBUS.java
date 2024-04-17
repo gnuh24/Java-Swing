87c8982d1ca808fce7aa91384094e6f2ce87b369
MinhVi2004 <141652938+MinhVi2004@users.noreply.github.com>
2024-04-17 17:46:19 +0700

    Update PhieuXuatKhoBUS.java

--- JavaSwingProject/src/main/java/BUS/PhieuXuatKhoBUS.java
+++ JavaSwingProject/src/main/java/BUS/PhieuXuatKhoBUS.java
@@ -4,9 +4,11 @@ import DAO.*;
 import java.util.ArrayList;
 
 import DTO.NghiepVuXuatKho.*;
+import DTO.ThongTinSanPham.*;
 
 public class PhieuXuatKhoBUS {
     private final PhieuXuatKhoDAO PhieuXuatKhoDAO= new PhieuXuatKhoDAO();
+    private SanPhamBUS danhSachSanPhamBUS = new SanPhamBUS();
 
     public PhieuXuatKhoBUS() {
     }
@@ -30,7 +32,49 @@ public class PhieuXuatKhoBUS {
     public int maPhieuXuatKhoTiepTheo() {
         return PhieuXuatKhoDAO.maPhieuXuatKhoTiepTheo();
     }
-    
-    
+    public ArrayList<PhieuXuatKhoDTO> search(int index){
+        ArrayList<PhieuXuatKhoDTO> kq= new ArrayList<>();
+        switch (index) {
+            case 0:
+                for(PhieuXuatKhoDTO sp: this.getAll(4)){
+                        kq.add(sp);
+                }
+                break;
+            case 1:
+                for(PhieuXuatKhoDTO sp: this.getAll(4)){
+                    if( sp.getTongGiaTri() <=  5000000)
+                        kq.add(sp);
+                }
+                break;
+            case 2:
+                for(PhieuXuatKhoDTO sp: this.getAll(4)){
+                    if( sp.getTongGiaTri() >  5000000 && sp.getTongGiaTri() <=  10000000)
+                        kq.add(sp);
+                }
+                break;
+            case 3:
+                for(PhieuXuatKhoDTO sp: this.getAll(4)){
+                    if( sp.getTongGiaTri() >  10000000 && sp.getTongGiaTri() <=  20000000)
+                        kq.add(sp);
+                }
+                break;
+            case 4:
+                for(PhieuXuatKhoDTO sp: this.getAll(4)){
+                    if( sp.getTongGiaTri() >  20000000 && sp.getTongGiaTri() <=  40000000)
+                        kq.add(sp);
+                }
+                break;
+            case 5:
+                for(PhieuXuatKhoDTO sp: this.getAll(4)){
+                    if( sp.getTongGiaTri() >  40000000)
+                        kq.add(sp);
+                }
+                break;
+        
+            default:
+                break;
+        }
+        return kq;
+    }
     
 }
