ebfe720d7538294065960198734f55cd6b584b66
Thuy An <128052804+thuy0an@users.noreply.github.com>
2024-04-19 19:57:47 +0700

    hoàn thành chức năng SP&LoạiSP

--- JavaSwingProject/src/main/java/BUS/ThongTinSanPham/SanPhamBUS.java
+++ JavaSwingProject/src/main/java/BUS/ThongTinSanPham/SanPhamBUS.java
@@ -2,14 +2,17 @@ package BUS.ThongTinSanPham;
 
 import java.util.ArrayList;
 
-import DAO.SanPhamDAO;
+import DAO.ThongTinSanPham.SanPhamDAO;
 import DTO.ThongTinSanPham.SanPhamDTO;
+import java.util.Collections;
+import java.util.Comparator;
 
 
 public class SanPhamBUS {
     private final SanPhamDAO sanPhamDAO= new SanPhamDAO();
     private ArrayList<SanPhamDTO> danhSachSanPham= new ArrayList<>();
     private LoaiSanPhamBUS loaiSPBUS= new LoaiSanPhamBUS();
+    private int maKho;
     
     public SanPhamBUS() {
         danhSachSanPham=sanPhamDAO.getAll(0);
@@ -17,7 +20,7 @@ public class SanPhamBUS {
     
     public ArrayList<SanPhamDTO> getAll(){
         
-        danhSachSanPham=sanPhamDAO.getAll(0);
+        danhSachSanPham=sanPhamDAO.getAll(0); 
         return this.danhSachSanPham;
     }
     // hàm hỗ trợ lấy ra index trong danh sách sản phẩm dành cho việc update thông tin sản phẩm
@@ -43,22 +46,21 @@ public class SanPhamBUS {
             if(this.danhSachSanPham.get(i).getTenSanPham().equals(spMoi.getTenSanPham()))
                 return false;
         
-        if( sanPhamDAO.create(0,spMoi))
+        if( sanPhamDAO.create(1,spMoi))
         {
             this.danhSachSanPham.add(spMoi);
             return true;
         }
         return false;
     }
-
-    public boolean updateSoLuongSanPham(SanPhamDTO dto){
-        return sanPhamDAO.updateSoLuongSanPham(dto);
-    }
     
     
     
     public boolean update(SanPhamDTO spMoi){
-        if( sanPhamDAO.update(spMoi)){
+        SanPhamDTO spdto= sanPhamDAO.getByName(spMoi.getMaKhoHang(),spMoi.getTenSanPham());
+        if(spdto!=null && spdto.getMaSanPham() != spMoi.getMaSanPham())
+            return false;
+        else if( sanPhamDAO.update(spMoi)){
             this.danhSachSanPham.set(getIndexByMaSP(spMoi.getMaSanPham()), spMoi);
             return true;
         }
@@ -66,10 +68,15 @@ public class SanPhamBUS {
         
     }
     
+    public boolean updateSoLuongSanPham(SanPhamDTO dto){
+        return sanPhamDAO.updateSoLuongSanPham(dto);
+    }
+    
+    
     public boolean delete(SanPhamDTO spCanXoa){
         if( sanPhamDAO.delete(spCanXoa))
         {
-            this.danhSachSanPham.remove(spCanXoa);
+            this.danhSachSanPham=sanPhamDAO.getAll(0);
             return true;
         }
         return false;
@@ -87,4 +94,64 @@ public class SanPhamBUS {
         }
         return kq;
     }
+    public ArrayList<SanPhamDTO> locTheoGiaTangGiam(String txt){
+        if (txt.equals("Tăng"))
+        {
+            Collections.sort(this.danhSachSanPham, new Comparator<SanPhamDTO>() {
+                @Override
+                public int compare(SanPhamDTO sp1, SanPhamDTO sp2) {
+                    return Double.compare(sp1.getGiaSanPham(), sp2.getGiaSanPham());
+                }
+            });
+        }
+        else {
+                Collections.sort(this.danhSachSanPham, new Comparator<SanPhamDTO>() {
+                @Override
+                public int compare(SanPhamDTO sp1, SanPhamDTO sp2) {
+                    return Double.compare(sp2.getGiaSanPham(), sp1.getGiaSanPham());
+                }
+            });
+        }
+        return this.danhSachSanPham;
+        
+    }
+    public ArrayList<SanPhamDTO> locTheoKhoangGia(int loai,int num1, int num2){
+        ArrayList<SanPhamDTO> kq= new ArrayList<>();
+        switch (loai) {
+            case 1:
+            for(SanPhamDTO sp: this.danhSachSanPham){
+                if( sp.getGiaSanPham() < 200000)
+                    kq.add(sp);
+            }    
+                break;
+            case 2:
+            for(SanPhamDTO sp: this.danhSachSanPham){
+                if( sp.getGiaSanPham() > 200000 && sp.getGiaSanPham() < 1000000)
+                    kq.add(sp);
+            }
+                break;
+            case 3:
+            for(SanPhamDTO sp: this.danhSachSanPham){
+                if( sp.getGiaSanPham() > 1000000 && sp.getGiaSanPham() < 2000000)
+                    kq.add(sp);
+            }
+                break;
+            case 4:
+            for(SanPhamDTO sp: this.danhSachSanPham){
+                if( sp.getGiaSanPham() > 2000000)
+                    kq.add(sp);
+            }
+                break;
+            case 5:
+            for(SanPhamDTO sp: this.danhSachSanPham){
+                if( sp.getGiaSanPham() > num1 && sp.getGiaSanPham() < num2)
+                    kq.add(sp);
+            }
+                break;
+        }
+        return kq;
+        
+    }
+
+    
 }
